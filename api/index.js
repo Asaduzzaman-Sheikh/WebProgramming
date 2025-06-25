cf18422f07275f8bf41f87e99f39d9cc47f2db60
AsaduzzamanSheikh <asaduzzamansheikh.web777@gmail.com>
2025-06-25 12:57:24 +0600

    Add dotenv and mongoose dependencies; configure MongoDB connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// console.log('MONGO_URL:', process.env.MONGO_URL);  // Add this line
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('✅ Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('❌ MongoDB connection failed:', error.message);
+  });
+
+
+import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
